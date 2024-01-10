0d083120628669cef4aa32d814ac6f1d6fac7ec2
francoromeo <francoromeo5012@gmail.com>
2024-01-10 11:37:21 -0300

    scroll

--- js/app.js
+++ js/app.js
@@ -25,3 +25,39 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+ScrollReveal().reveal(".top-nav", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+});
+
+ScrollReveal().reveal(".nav", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+    delay: 100,
+});
+
+ScrollReveal().reveal(".header", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+    delay: 200,
+});
+
+ScrollReveal().reveal(".section", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+    duration: 1000,
+    delay: 200,
+});
+
+ScrollReveal().reveal(".footer", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+    duration: 1000,
+    delay: 200,
+});
